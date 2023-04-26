d9d93636597737a4bc5c99db8e2409570e2486c2
Joebim <josey359gmail.com>
2023-04-25 20:10:13 -0700

    add files

--- _printf.c
+++ _printf.c
@@ -1,95 +1,66 @@
 #include "main.h"
-/**
- * print_char - print char arguments
- * @args: va_list argument
- * Return: return char number
- */
-int print_char(va_list args)
-{
-	_putchar(va_arg(args, int));
-	return (1);
-}
 
-/**
- * print_string - print string arguments
- * @args: va_list argument
- * Return: return the string
- */
-int print_string(va_list args)
-{
-	char *str = va_arg(args, char *);
-	int i;
-	int count = 0;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-		count++;
-	}
-	return (count);
-}
-
-/**
- * print_percent - print the percent argument
- * @args: va_list argument
- * Return: return the percent
- */
-int print_percent(va_list args)
-{
-	(void) args;
-	_putchar('%');
-	return (1);
-}
+void print_buffer (char buffer[], int *buff_ind);
 
 /**
  * _printf - produces output to a format
  * @format: character string argument
  * Return: returns an int value
  */
+
 int _printf(const char *format, ...)
 {
-	int count;
-	int i;
-	va_list args;
-	int (*print_func[3])(va_list);
-	char specifiers[3];
+	int n, print_type = 0, char_print = 0;
+	int f, y, pre, s, buff_ind = 0;
+	va_list list;
+	char buffer(BUFF_SIZE);
 
-	va_start(args, format);
-	print_func[0] = print_char;
+	if (format == NULL)
+		return(-1);
 
-	print_func[1] = print_string;
-	
-	print_func[2] = print_percent;
+	va_start(list, format);
 
-	specifiers[0] = 'c';
-	
-	specifiers[1] = 's';
-	
-	specifiers[2] = '%';
-
-	count = 0;
-	for (i = 0; format[i] != '\0'; i++)
+	for (n = 0; format && format[n] != '\0'; n++)
 	{
-		if (format[i] == '%')
+		if (format[n] != '\0')
 		{
-			int j;
-
-			i++;
-			for (j = 0; j < 3; j++)
-			{
-				if (format[i] == specifiers[j])
-				{
-					count += print_func[j](args);
-					break;
-				}
-			}
+			buffer(buff_ind++) = format[n];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			char_print++;
 		}
+
 		else
 		{
-			_putchar(format[i]);
-			count++;
+			print_buffer(buffer, &buff_ind);
+			f = get_f(format, &n);
+			y = get_y(format, &n, list);
+			pre = get_s(format, &n);
+			s = get_s(format, &n);
+			++n;
+			print_type = handle_print(format, &n, list, buffer, f, y, pre, s);
+			if (print_type == -1)
+				return (-1);
+			char_print += print_type;
 		}
 	}
-	va_end(args);
-	return (count);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (char_print);
+}
+
+/**
+ * print_buffer - print the result of the buffer
+ * @buffer: character array
+ * @buff_ind: Index for character, showing lenght
+ */
+void print_buffer((char buffer[]), int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
